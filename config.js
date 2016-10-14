1c83d8fa11ede131a4676aad047e69e6b218b03d
Christian Lizell <christian.lizell@aftonbladet.se>
2016-10-14 15:42:55 +0200

    The config should be neutral

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ export const debug = true;
 
 export const error = true;
 
-export const subject = 'jullunch.check-in';
+export const subject = 'check-in';
 
-export const endpoint = 'http://10.121.126.181:1337/events';
+export const endpoint = '<endpoint>';
 export const endpointMethod = 'POST';
 export const endpointQueryTemplate = '?subj=${subject}&rfid=${rfid}';
 
-export const apiKey = 'rebel-alliance';
+export const apiKey = '';
