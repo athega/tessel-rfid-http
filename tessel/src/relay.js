519d28a1b15e72364cfd399bb247a456bdbf2ecf
Christian Lizell <christian.lizell@aftonbladet.se>
2016-12-09 17:43:38 +0100

    Fixes error logging, some linting and skipping if not ready

--- tessel/src/relay.js
+++ tessel/src/relay.js
@@ -1,13 +1,20 @@
 import tessel from 'tessel';
 import relaylib from 'relay-mono';
-import log from './logger';
+import log, { error } from './logger';
 
-const relay = relaylib.use(tessel.port['B']);
+const relay = relaylib.use(tessel.port.B);
+
+let READY = false;
 
 export const triggerFlash = () => {
+    if (!READY) {
+        log('Not triggering flash on relay module. Device not ready.');
+        return;
+    }
+
     log('Trigger flash on relay module.');
 
-    relay.turnOn(1, (err) => {
+    relay.turnOn(1, () => {
         log('Relay on');
 
         setTimeout(() => {
@@ -20,10 +27,11 @@ export const triggerFlash = () => {
 
 relay.on('ready', () => {
     log('Relay is ready');
+    READY = true;
 });
 
 relay.on('error', (err) => {
-    log('Relay error', err)
+    error('Relay error', err);
 });
 
 relay.on('latch', (channel, value) => {
